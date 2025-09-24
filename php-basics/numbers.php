38975e5c1bbbeedbf1661fa0e35a662130d73781
Tiberiu Ion <tiberiu.vi@gmail.com>
2025-09-24 21:40:38 +0100

    Numbers

---
+++ php-basics/numbers.php
@@ -0,0 +1,8 @@
+<?php
+
+// Numbers
+// Surprisingly, this lessong doesn't cover much, just what you see below.
+
+$skillLevel = 100;
+$confidence = 0.5;
+$confidence = 5E-1; // scientific notation.
