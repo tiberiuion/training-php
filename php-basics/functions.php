8e44a66ddd0d8e94ab68ffa850380347c48bd6cc
Tiberiu Ion <tiberiu.vi@gmail.com>
2025-09-25 22:10:48 +0100

    Functions data types and globals

--- php-basics/functions.php
+++ php-basics/functions.php
@@ -18,3 +18,38 @@ echo hello('World');
 // TODO: Learn more about functions.
 // https://www.php.net/manual/en/language.functions.php
 ?>
+
+<h2>Functions with data types</h2>
+<?php 
+// This is new to me.
+
+// You can declare the data type for parameters and return statements.
+// This becomes useful in larger codebases. It helps you see what value types functions take as input and what they output.
+function foo(string $bar): string {
+    return  $bar;
+}
+// If you call this using anyting but a string it will throw a type error.
+echo foo("Hello function with data types.");
+?>
+
+<h2>Global variables</h2>
+<?php 
+// Follow the steps 1 2 3 GO!
+
+// 1. We define a global variable
+    $title = 'Hello world';
+
+    function divTitle() {
+        // 3. There are 2 ways to import the global variable inside the local scope
+        // global $title;
+        $title = $GLOBALS['title']; // Define the title variable locally using the suberglobal variable.
+
+        // 2. We try to access it inside the function but computer says no.
+        return "<div>$title</div>";
+    }
+
+    // TODO: learn more about globals and superglobals.
+
+echo divTitle();
+
+?>
